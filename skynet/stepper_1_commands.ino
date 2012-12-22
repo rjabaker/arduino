382226b1ece370de821d0164d4e5607aa00ab4e1
rjabaker <rjabaker@outlook.com>
2012-12-21 19:11:05 -0800

    No obvious changes.

--- skynet/stepper_1_commands.ino
+++ skynet/stepper_1_commands.ino
@@ -3,7 +3,7 @@ void ExecuteSetStepper1Command(byte *input)
   // Stepper command: [command ID][speed in rpm][steps]
   stepper1.setSpeed(input[1]);
   stepper1.step(input[2]);
-  delay(100);
+  delay(50);
   
   // RBakerFlag -> Should return some information!
 }
